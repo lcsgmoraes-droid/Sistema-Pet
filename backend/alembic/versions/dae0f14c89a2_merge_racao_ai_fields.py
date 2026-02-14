9cad601f21b79c6bef4f4e8306991d1ca6b1c510
Lucas Guerra <lcsgmoraes@gmail.com>
2026-02-14 20:25:20 -0300

    fix: remover migrations 20260214 com referencias quebradas

---
+++ backend/alembic/versions/dae0f14c89a2_merge_racao_ai_fields.py
@@ -0,0 +1,28 @@
+"""merge_racao_ai_fields
+
+Revision ID: dae0f14c89a2
+Revises: 20260214_add_racao_ai_fields, 673f25f32970
+Create Date: 2026-02-14 03:34:55.031226
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'dae0f14c89a2'
+down_revision: Union[str, Sequence[str], None] = ('20260214_add_racao_ai_fields', '673f25f32970')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
